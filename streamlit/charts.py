48dc2baa78b7866cdf3c36d693ab8a1919079435
Pablo <ramoswilkinspj28@gmail.com>
2023-07-04 22:59:02 -0500

    experimentando con streamlit

---
+++ streamlit/charts.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+cantidad_por_tipo = {'Americano': 40000, 'Mozzarella': 10000, 'Lite Line': 20000}
+st.title('Producto Almacenado:tada:')
+
+
+col1, col2 = st.columns(2)
+with col1:
+    st.metric(label="Capacidad del almacén", value='75%', delta='454')
+    st.metric(label="Capacidad del almacén", value='75%', delta='454')
+with col2:
+    st.subheader('Bar Chart')
+    st.write("Este es un ejemplo para ver como se ve streamlit")
+    st.bar_chart(cantidad_por_tipo)
+
